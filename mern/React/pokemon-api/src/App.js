f9d1745582e30bd21c5840d5c47528a605cd5139
Nouf-Al <nouffawazz@gmai.com>
2020-12-01 04:03:53 +0300

    Pokemon API

---
+++ mern/React/pokemon-api/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Api from './components/api';
+
+function App() {
+  return (
+    <>
+    <Api/>
+    </>
+  );
+}
+
+export default App;
---
+++ mern/React/pokemon-api/src/components/api.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+
+
+const Api = (props) => {
+    const [pokemons, setPokemons] = useState([]);
+
+    
+
+    const clickHandeler = () =>{
+        
+            fetch('https://pokeapi.co/api/v2/pokemon?limit=807')
+                .then(response => response.json())
+                .then(response => setPokemons(response.results))
+
+    };
+    
+    return (
+        <>
+        <button onClick={clickHandeler}>Fetch Pokemons</button>
+        <div>
+            {pokemons.length > 0 && pokemons.map((pokemon, index) => {
+                return (<div key={index}>{pokemon.name}</div>)
+            })}
+            </div>
+        </>
+    );
+
+}
+export default Api;
