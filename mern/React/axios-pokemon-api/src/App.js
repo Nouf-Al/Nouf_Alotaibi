39ba862c3baf738a81b21a31a59db2d990313a1b
Nouf-Al <nouffawazz@gmai.com>
2020-12-01 11:38:55 +0300

    Axios Pokemon API

---
+++ mern/React/axios-pokemon-api/src/App.js
@@ -0,0 +1,33 @@
+import React , {useState} from 'react';
+import axios from 'axios';
+
+
+function App() {
+  const [pokemons, setPokemons] = useState([]);
+
+    
+
+    const clickHandeler = () =>{
+        
+      axios.get('https://pokeapi.co/api/v2/pokemon?limit=807')
+        .then(response=>{setPokemons(response.data.results)})
+        .catch(err=>{
+          console.log(err);
+      });
+
+    }
+    
+    return (
+        <>
+        <button onClick={clickHandeler}>Fetch Pokemons</button>
+        <div>
+            {pokemons.map((pokemon, index) => {
+                return (<div key={index}>{pokemon.name}</div>)
+            })}
+            </div>
+        </>
+    );
+
+}
+
+export default App;
