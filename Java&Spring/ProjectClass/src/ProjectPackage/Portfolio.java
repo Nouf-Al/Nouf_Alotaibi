c81cfda4ec388036dbabbaf9dbeb7e1a1f6b16fa
Nouf-Al <nouffawazz@gmai.com>
2021-01-05 04:12:29 +0300

    Project Class

---
+++ Java&Spring/ProjectClass/src/ProjectPackage/Portfolio.java
@@ -0,0 +1,24 @@
+package ProjectPackage;
+
+import java.util.ArrayList;
+
+public class Portfolio {
+	ArrayList<Project> Projects ;
+	
+	public Portfolio() {
+		this.Projects = new ArrayList<Project>();
+	}
+	public void AddToPortfolio(Project p) {
+        Projects.add(p);
+    }
+    
+    public void showPortfolio() {
+        double total = 0;
+        for (Project p : Projects) {
+            total = total + p.getCost();
+            System.out.println(p.ElevatorPitch());
+        }
+        System.out.println(String.format("Total Cost: "+ total));
+    }
+
+}
